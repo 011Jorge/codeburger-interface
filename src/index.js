f94f269bc46e1b46cedd4287967cd723e2ad2c3f
011Jorge <vicentejorge187@gmail.com>
2022-02-24 20:58:59 -0300

    Global styles with Styled Components

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Login from './containers/Login'
+import GlobalStyle from './styles/globalStyle'
 
-ReactDOM.render(<Login />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <Login />,
+    <GlobalStyle />
+  </>,
+
+  document.getElementById('root')
+)
