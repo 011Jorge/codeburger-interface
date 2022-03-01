b069035af95949ad6412e3754eb1280f9717a4de
011Jorge <vicentejorge187@gmail.com>
2022-02-28 22:49:37 -0300

    Creating Registration screen and creating a user

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Login from './containers/Login'
+import Register from './containers/Register'
 import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
