1a39586a495fe83b8a6f177e022f1cc88ba06a17
011Jorge <vicentejorge187@gmail.com>
2022-04-29 23:35:49 -0300

    bug solved

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3003'
+  baseURL: 'http://localhost:3005'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
