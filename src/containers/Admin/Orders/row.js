0edabb11cef44e428c90dc40e725804d2c707a96
011Jorge <vicentejorge187@gmail.com>
2022-05-19 13:28:45 -0300

    Updating order status

--- src/containers/Admin/Orders/row.js
+++ src/containers/Admin/Orders/row.js
@@ -1,23 +1,41 @@
-import React, { useEffect, useState } from "react"; 
+import React from 'react'
 
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import Box from '@mui/material/Box'
+import Collapse from '@mui/material/Collapse'
+import IconButton from '@mui/material/IconButton'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Typography from '@mui/material/Typography'
+import PropTypes from 'prop-types'
 
-import { ProductsImg } from './styles'
+import api from '../../../services/api'
+import status from './order-status'
+import { ProductsImg, ReactSelectStyle } from './styles'
 
-function Row({row}){
-  const [open, setOpen] = React.useState(false);
+function Row({ row, setOrders, orders }) {
+  const [open, setOpen] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  async function setNewStatus(id, status) {
+    setIsLoading(true)
+    try {
+      await api.put(`orders/${id}`, { status })
+
+      const newOrders = orders.map(order => {
+        return order._id === id ? { ...order, status } : order
+      })
+      setOrders(newOrders)
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <React.Fragment>
@@ -36,8 +54,20 @@ function Row({row}){
         </TableCell>
         <TableCell>{row.name}</TableCell>
         <TableCell>{row.date}</TableCell>
-        <TableCell>{row.status}</TableCell>
-        <TableCell>{ }</TableCell>
+        <TableCell>
+          <ReactSelectStyle
+            options={status.filter(sts => sts.value !== 'Todos')}
+            menuPortalTarget={document.body}
+            placeholder="Status"
+            defaultValue={
+              status.find(option => option.value === row.status) || null
+            }
+            onChange={newStatus => {
+              setNewStatus(row.orderId, newStatus.value)
+            }}
+            isLoading={isLoading}
+          />
+        </TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -56,7 +86,7 @@ function Row({row}){
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.products.map((productRow) => (
+                  {row.products.map(productRow => (
                     <TableRow key={productRow.id}>
                       <TableCell component="th" scope="row">
                         {productRow.quantity}
@@ -64,7 +94,10 @@ function Row({row}){
                       <TableCell>{productRow.name}</TableCell>
                       <TableCell>{productRow.category}</TableCell>
                       <TableCell>
-                        <ProductsImg src={productRow.url} alt="image-produto"/>
+                        <ProductsImg
+                          src={productRow.url}
+                          alt="imagem-do-produto"
+                        />
                       </TableCell>
                     </TableRow>
                   ))}
@@ -75,11 +108,26 @@ function Row({row}){
         </TableCell>
       </TableRow>
     </React.Fragment>
-  );
-
-    return (
-        <Container>
-        </Container>
-    )
+  )
 }
+
+Row.propTypes = {
+  orders: PropTypes.array,
+  setOrders: PropTypes.func,
+  row: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    orderId: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    products: PropTypes.arrayOf(
+      PropTypes.shape({
+        quantity: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired
+      })
+    ).isRequired
+  }).isRequired
+}
+
 export default Row
