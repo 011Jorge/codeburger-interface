f5e7fcdf214e8a7c8a08d62557474d52d7e98854
011Jorge <vicentejorge187@gmail.com>
2022-05-21 19:35:09 -0300

    Creating logout Admin

--- src/components/SlideMenuAdmin/index.js
+++ src/components/SlideMenuAdmin/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import { useUser } from '../../hooks/UserContext'
 import { Container, ItemContainer, Links } from './styles'
 import listLinks from './menu-list'
 
 export function SlideMenuAdmin() {
+  const { logout } = useUser() 
   return (
     <Container>
       <hr />
       {listLinks.map( item => (
-        <ItemContainer key={item.id}>
+        <ItemContainer key={item.id} isActive={true}>
           <item.icon className='icon' />
           <Links to={item.link}>{item.label}</Links>
         </ItemContainer>
       ))}
       <hr />
+      <ItemContainer style={{ position: 'absolute', bottom: '30px'}}>
+        <LogoutIcon style={{color: '#fff'}} />
+        <Links to={"/login"} onClick={logout}>Sair</Links>
+      </ItemContainer>
     </Container>
   ) 
 }
