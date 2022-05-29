9655ec7b493eedae30f7360f6627b1066a140cd5
011Jorge <vicentejorge187@gmail.com>
2022-05-29 12:35:37 -0300

    Arranging Active Link in Admin Menu

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -4,15 +4,23 @@ import { Container, ContainerItems } from './styles'
 import Orders from './Orders'
 import ListProducts from "./ListProducts";
 import { SlideMenuAdmin } from "../../components/SlideMenuAdmin";
+import PropTypes from 'prop-types'
+import paths from "../../constants/paths";
 
-export function Admin(){
+export function Admin({ match: { path } }){
     return (
         <Container>
             <SlideMenuAdmin />  
             <ContainerItems>
-                {/* <Orders /> */}
-                <ListProducts />
+                {path === paths.Order && <Orders /> }
+                {path === paths.ListProducts && <ListProducts /> }
             </ContainerItems>
         </Container>
     )
+}
+
+Admin.propTypes = {
+    match: PropTypes.shape({
+        path: PropTypes.string
+    })
 }
