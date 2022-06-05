a4e387a355770ed05b874481f56037aaa57f5f92
011Jorge <vicentejorge187@gmail.com>
2022-06-05 20:52:52 -0300

    Validating product creation form

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactSelect from "react-select";
 
-import { Button } from "../../../components/Button";
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Button, ErrorMessage } from "../../../components";
 import apiCodeBurger from "../../../services/api";
 import { Container, Label, Input, LabelUpload } from './styles'
 import { useForm, Controller } from "react-hook-form";
@@ -11,27 +14,49 @@ function NewProduct() {
   const [fileName, setFileName] = useState(null)
   const [categories, setCategories] = useState([])
 
-  const { register, handleSubmit, control } = useForm();
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Digite o nome do produto'),
+    price: Yup.string().required('Digite o preço do produto'),
+    category: Yup.object().required('Escolha uma categoria'),
+    file: Yup.mixed().test('required', 'Carregue um arquivo', value => {
+      return value?.length > 0
+    }).test('fileSize', 'Carregue arquivos de até 2MB', value => {
+      return value[0]?.size <= 200000
+    }).test('type', 'Carregue apenas arquivos JPEG', value => {
+      return value[0]?.type === 'image/jpeg' || value[0]?.type === 'image/png'
+    })
+  })
+
+  const { 
+    register, 
+    handleSubmit, 
+    control, 
+    formState: { errors } 
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
   const onSubmit = data => console.log(data);
 
   useEffect(() => {
     async function loadCategories() {
       const { data } = await apiCodeBurger.get("categories");
 
-      console.log(data)
       setCategories(data)
     }
     loadCategories();
   }, []);
-
+  
   return (
     <Container>
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <Label>Nome</Label>
         <Input type="text" {...register("name")}/>
+        <ErrorMessage>{errors.name?.message}</ErrorMessage>
 
         <Label>Preço</Label>
         <Input type="number" {...register("price")}/>
+        <ErrorMessage>{errors.price?.message}</ErrorMessage>
 
         <LabelUpload>
           { fileName || (
@@ -49,6 +74,7 @@ function NewProduct() {
             }}
           />
         </LabelUpload>
+        <ErrorMessage>{errors.file?.message}</ErrorMessage>
 
         <Controller
           name="category_id"
@@ -65,6 +91,7 @@ function NewProduct() {
             )
           }}  
         ></Controller>
+        <ErrorMessage>{errors.category?.message}</ErrorMessage>
 
         <Button style={{ 'width': '100%', 'marginTop': '25px'}}>
           Adicionar produtos
