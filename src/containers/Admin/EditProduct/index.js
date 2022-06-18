682fea5e3dc26f0e9b7307a275405e03c3b154b1
011Jorge <vicentejorge187@gmail.com>
2022-06-18 18:31:56 -0300

    finalizando a tela de edição de usuários

--- src/containers/Admin/EditProduct/index.js
+++ src/containers/Admin/EditProduct/index.js
@@ -1,50 +1,49 @@
-import React, { useEffect, useState } from "react";
-import ReactSelect from "react-select";
-
-import * as Yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-
-import { Button, CardProducts, ErrorMessage } from "../../../components";
-import apiCodeBurger from "../../../services/api";
-import { Container, Label, Input, LabelUpload } from './styles'
-import { useForm, Controller } from "react-hook-form";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import React, { useEffect, useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
+import ReactSelect from 'react-select'
 import { toast } from 'react-toastify'
-import { useHistory } from 'react-router-dom';
 
-function NewProduct() {
+import { yupResolver } from '@hookform/resolvers/yup'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import * as Yup from 'yup'
+
+import { ErrorMessage } from '../../../components'
+import api from '../../../services/api'
+import {
+  Container,
+  Label,
+  Input,
+  ButtonStyles,
+  LabelUpload,
+  ContainerInput
+} from './styles'
+
+function EditProduct() {
   const [fileName, setFileName] = useState(null)
   const [categories, setCategories] = useState([])
-  const { 
-    push, 
+  const {
+    push,
     location: {
-      state: {product}
+      state: { product }
     }
   } = useHistory()
-
-  console.log(product)
 
   const schema = Yup.object().shape({
     name: Yup.string().required('Digite o nome do produto'),
     price: Yup.string().required('Digite o preço do produto'),
     category: Yup.object().required('Escolha uma categoria'),
-    file: Yup.mixed().test('required', 'Carregue um arquivo', value => {
-      return value?.length > 0
-    }).test('fileSize', 'Carregue arquivos de até 2MB', value => {
-      return value[0]?.size <= 200000
-    }).test('type', 'Carregue apenas arquivos JPEG', value => {
-      return value[0]?.type === 'image/jpeg' || value[0]?.type === 'image/png'
-    })
+    offer: Yup.bool()
   })
 
-  const { 
-    register, 
-    handleSubmit, 
-    control, 
-    formState: { errors } 
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
-  });
+  })
 
   const onSubmit = async data => {
     const productDataFormData = new FormData()
@@ -53,89 +52,109 @@ function NewProduct() {
     productDataFormData.append('price', data.price)
     productDataFormData.append('category_id', data.category.id)
     productDataFormData.append('file', data.file[0])
+    productDataFormData.append('offer', data.offer)
 
-    await toast.promise( apiCodeBurger.post('products', productDataFormData), {
-      pending: 'Criando novo produto...',
-      sucess: 'Produto criado com sucesso',
-      error: 'Falha ao tentar criar o produto'
-    })
+    await toast.promise(
+      api.put(`products/${product.id}`, productDataFormData),
+      {
+        pending: 'Editando novo produto...',
+        success: 'Produto editado com sucesso',
+        error: 'Falha ao editar o produto'
+      }
+    )
 
     setTimeout(() => {
       push('/listar-produtos')
-    }, 2000);
+    }, 2000)
   }
-  
 
   useEffect(() => {
     async function loadCategories() {
-      const { data } = await apiCodeBurger.get("categories");
-
+      const { data } = await api.get('categories')
       setCategories(data)
     }
-    loadCategories();
-  }, []);
-  
+    loadCategories()
+  }, [])
+
   return (
     <Container>
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <div>
           <Label>Nome</Label>
-          <Input type="text" {...register("name")}/>
+          <Input
+            type="text"
+            {...register('name')}
+            defaultValue={product.name}
+          />
           <ErrorMessage>{errors.name?.message}</ErrorMessage>
         </div>
 
         <div>
           <Label>Preço</Label>
-          <Input type="number" {...register("price")}/>
+          <Input
+            type="number"
+            {...register('price')}
+            defaultValue={product.price}
+          />
           <ErrorMessage>{errors.price?.message}</ErrorMessage>
         </div>
 
         <div>
           <LabelUpload>
-            { fileName || (
+            {fileName || (
               <>
                 <CloudUploadIcon />
-                Carregue a imagem do produto  
+                Carregue a imagem do produto
               </>
             )}
-            <input 
-              type="file" 
-              accept="image/png, image/jpeg" 
-              {...register("file")}
+
+            <input
+              type="file"
+              id="image-input"
+              accept="image/png, image/jpeg"
+              {...register('file')}
               onChange={value => {
-                setFileName(value.target.files[0].name)
+                setFileName(value.target.files[0]?.name)
               }}
             />
           </LabelUpload>
           <ErrorMessage>{errors.file?.message}</ErrorMessage>
         </div>
-        
+
         <div>
           <Controller
             name="category"
             control={control}
+            defaultValue={product.category}
             render={({ field }) => {
               return (
-                <ReactSelect 
-                {...field}
-                options={categories}
-                getOptionLabel={ cat => cat.name}
-                getOptionValue={ cat => cat.id}
-                placeholder="Categorias"
-              />
+                <ReactSelect
+                  {...field}
+                  options={categories}
+                  getOptionLabel={cat => cat.name}
+                  getOptionValue={cat => cat.id}
+                  placeholder="Categorias"
+                  defaultValue={product.category}
+                />
               )
-            }}  
+            }}
           ></Controller>
           <ErrorMessage>{errors.category?.message}</ErrorMessage>
         </div>
 
-        <Button style={{ 'width': '100%', 'marginTop': '25px'}}>
-          Adicionar produtos
-        </Button>
-        
+        <ContainerInput>
+          <input
+            type="checkbox"
+            {...register('offer')}
+            defaultChecked={product.offer}
+          />
+          <Label>Produto em oferta ?</Label>
+        </ContainerInput>
+
+        <ButtonStyles>Editar produto</ButtonStyles>
       </form>
-    </Container>  
-  );
+    </Container>
+  )
 }
 
-export default NewProduct;
+export default EditProduct
